17246b9faad86d7c6a27f8baa5b9a3bfcdf5ad6e
Hemanth Kumar AP <hemanth@codelattice.com>
2013-08-05 19:54:58 +0530

    Core module update

--- ecm_core/models.py
+++ ecm_core/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Mail_address(models.Model):
+    class Meta:
+        verbose_name = 'email address'
+        verbose_name_plural = 'email adresses'
+    mail_list = models.ForeignKey('Mailing_list')
+    mail_id = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    def __unicode__(self):
+        return self.mail_id
+
+class Mailing_list(models.Model):
+    class Meta:
+        verbose_name = 'mailing list'
+        verbose_name_plural = 'mailing lists'
+    title = models.CharField(max_length=50)
+    date_of_creation = models.DateTimeField(editable=False, default=now())
+    def __unicode__(self):
+        return self.title
+	#client
+
+class campaign(models.Model):
+    class Meta:
+        verbose_name = 'campaign'
+        verbose_name_plural = 'campaigns'
+    subject      = models.CharField(max_length=100,help_text='Email Subject.')
+    sender       = models.EmailField(blank=False, null=True,help_text='Sender\'s Email ID. ( eg:info@gmail.com)')
+    mailing_list = models.ManyToManyField(
+        'Mailing_list',
+        help_text='Select atleast one mailing list :  ',
+        blank=True, verbose_name='Target mailing lists',
+        #limit_choices_to={'subscribed': True}
+        )   
+    date_created = models.DateTimeField(editable=False, default=now())
+    html = models.TextField(
+        verbose_name='Content', help_text=('Email body'),
+        null=True, blank=True
+    )
+    #status       = models.BooleanField(default=False)
+    def __unicode__(self):
+        return self.subject
