e6735b2f4e60d73b617444e1faf9e7cc58a3db1c
Hemanth Kumar AP <hemanth@codelattice.com>
2013-09-10 23:03:27 +0530

    Change of name , mailing lsit - > groups Delete conformation for templates Fixing name inconsistency bug in open track

--- ecm_core/forms.py
+++ ecm_core/forms.py
@@ -7,27 +7,22 @@ from models import Mail_address, Mailing_list, campaign, mailtemplate
 from django.contrib.admin.widgets import FilteredSelectMultiple
 
 
+#NEEDED
 class Importform(forms.Form):
 	csv_file = forms.FileField(label="Select CSV file")
-	name = forms.CharField(label="Mailing list name")
+	name = forms.CharField(label="Group name")
 	#mailing_list = forms.ModelChoiceField(queryset=Mailing_list.objects.all())
-
-class campainform(forms.Form):
-	t_file = forms.FileField(label="Click to Browse E-mail template")
-	subj = forms.CharField(label="Subject")
-	#name = forms.CharField(label="Sender name", max_length=30,required=False)
-	#email_id= forms.EmailField(label="Sender email id",required=True)
 
 class addcform(forms.Form):
 	cat_name = forms.CharField(label="New category name", max_length=30,required=True)
 
-
+#NEEDED
 class ListBasketForm(forms.ModelForm): 
     CHOICES = (('T', 'Template',),('P', 'Plain text',)) #,  ('W', 'Wysisyg editor',)
     content_type = forms.ChoiceField(widget=forms.RadioSelect(
     attrs={'onclick': 'content_select(value);'},)
     , choices=CHOICES)
-    mailing_list = forms.ModelMultipleChoiceField(queryset=Mailing_list.objects.all())
+    mailing_list = forms.ModelMultipleChoiceField(queryset=Mailing_list.objects.all(),label='Target Groups')
     template = forms.ModelChoiceField(queryset=mailtemplate.objects.all(),empty_label=None)
     #html = forms.CharField(widget=forms.Textarea(attrs={'rows':'10', 'cols': '30'}))
 
@@ -38,7 +33,7 @@ class ListBasketForm(forms.ModelForm):
 class mailtemplateform(forms.ModelForm): 
     class Meta:
         model = mailtemplate
-        fields = ('name','thumbnail','zipfile')
+        fields = ('name','zipfile', 'thumbnail')
 
 class campselectform(forms.Form):
     campaign = forms.ModelChoiceField(queryset=campaign.objects.all().order_by("-date_created"),empty_label=None)
