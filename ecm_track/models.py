4c7fbb5be0367e134c0ae5d6314661b360dd5941
Hemanth Kumar AP <hemanth@codelattice.com>
2013-09-10 21:04:54 +0530

    Compensting absense of single name field

--- ecm_track/models.py
+++ ecm_track/models.py
@@ -5,7 +5,7 @@ class opens(models.Model):
     campaign_uuid = models.CharField(editable=False, max_length=100)
     template_uuid = models.CharField(editable=False, max_length=100)
     mail_id = models.CharField(max_length=30)
-    name = models.CharField(max_length=30, blank=True)
+    name = models.CharField(max_length=90, blank=True)
     date_of_creation = models.DateTimeField(editable=False, default=now())
     def __unicode__(self):
         return self.title
