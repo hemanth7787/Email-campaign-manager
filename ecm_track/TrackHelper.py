62558b0df57dc86fb7101756bc2f57ce405d48e5
Hemanth Kumar AP <hemanth@codelattice.com>
2013-09-11 13:32:21 +0530

    Bugfix: Object instance in place of str

--- ecm_track/TrackHelper.py
+++ ecm_track/TrackHelper.py
@@ -1,7 +1,7 @@
 import base64
 import pdb
 def TrackCode(host,mobj,cuuid):
-    params=str(mobj.mail_id+','+mobj.full_name+','+mobj.uid+','+cuuid)
+    params=str(mobj.mail_id+','+mobj.full_name()+','+mobj.uid+','+cuuid)
     encoded=base64.b64encode(params)
     code = '<img src="{0}" alt="" width="1" height="1" border="0" style="height:1px !important;width:1px\
     !important;border-width:0 !important;margin-top:0 !important;margin-bottom:0 !important;margin-right:0 \
