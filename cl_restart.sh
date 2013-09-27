dfe08ad40dfd5013f1852122d5a20bbd6241e1db
Hemanth Kumar AP <hemanth@codelattice.com>
2013-09-27 15:47:09 +0530

    Worker limits

--- cl_restart.sh
+++ cl_restart.sh
@@ -2,6 +2,6 @@
 ps auxww | grep 'celery worker' | awk '{print $2}' | xargs kill -9
 ps auxww | grep 'manage.py' | awk '{print $2}' | xargs kill -9
 source env/bin/activate
-nohup python manage.py celery worker -B --settings=email_campaign_manager.local_settings --loglevel=info > celery.log &
+nohup python manage.py celery worker -B --settings=email_campaign_manager.local_settings --loglevel=info --concurrency=1 > celery.log &
 nohup python manage.py runserver codestager.com:8050 --settings=email_campaign_manager.local_settings > django.log &
 
--- orange_restart.sh
+++ orange_restart.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-ps auxww | grep 'celery worker' | awk '{print $2}' | xargs kill -9
-ps auxww | grep 'manage.py' | awk '{print $2}' | xargs kill -9
 source /home/hemanth/working_ecm/env/bin/activate
-nohup python manage.py celery worker -B --settings=email_campaign_manager.local_settings --loglevel=info > celery.log &
+nohup python manage.py celery worker -B --settings=email_campaign_manager.local_settings --loglevel=info --concurrency=1 > celery.log &
 nohup python manage.py runserver codestager.com:8060 --settings=email_campaign_manager.local_settings > django.log &
 
