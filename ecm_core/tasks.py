90d87ce9f3268db579378c4d91a304538333ebe8
Hemanth <hemanth7787@gmail.com>
2014-11-24 23:35:41 +0530

    [Celery ETA fix] Celery scheduled task fixed.  Ensured campaign will run only once

--- ecm_core/tasks.py
+++ ecm_core/tasks.py
@@ -1,11 +1,11 @@
 from celery import task
 from bulkmailer import send_email
 from models import CampaignSchedule, SendgridEmailQuota
+import redis
+
 import logging
 logger = logging.getLogger("ecm_console")
-#import pdb
-#import time
-#from django.core.mail import EmailMultiAlternatives
+
 
 @task.task(ignore_result=True)
 def celery_sendmail_task(obj,unsubscribe_url,host):
@@ -28,10 +28,18 @@ def sendgrid_quota_reset():
 
 @task.task(ignore_result=True)
 def celery_scheduled_campaign(schedule_id):
+	# Lock to ensure task only runs once
+	LOCK_EXPIRE = 60 * 30 # Lock expires in 30 minutes
 	try:
 		obj = CampaignSchedule.objects.get(pk=schedule_id)
-		send_email(obj.campaign, obj.unsub_url, obj.ecm_host)
-		obj.campaign.status = True
-		obj.campaign.save()
+		my_lock = redis.Redis().lock(obj.campaign.campaign_uuid,timeout=LOCK_EXPIRE)
+		if my_lock.acquire(blocking=False) and obj.campaign.status == False:
+			send_email(obj.campaign, obj.unsub_url, obj.ecm_host)
+			obj.campaign.status = True
+			obj.campaign.save()
+			my_lock.release()
+		else:
+			logger.warn("Task is already running, Lock name:{0} ,Exiting".format(
+				obj.campaign.campaign_uuid))
 	except Exception, e:
 		logger.error("Critical Error : celery_scheduled_campaign: {0} ".format(e))
