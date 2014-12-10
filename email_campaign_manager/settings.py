635ca7299a827af6d9753286d40aab617b6b0e80
Hemanth <hemanth7787@gmail.com>
2014-12-10 15:42:05 +0530

    Ckedtor config updated

--- email_campaign_manager/settings.py
+++ email_campaign_manager/settings.py
@@ -18,17 +18,20 @@ ELFINDER_OPTIONS = {
 CKEDITOR_OPTIONS = {
     'skin': 'moono',
     'toolbar': [
-            [
-            '-', 'Bold', 'Italic', 'Underline', 'Strike','Subscript', 'Superscript', 'RemoveFormat'
-            '-', 'PasteText', 'PasteFromWord',
-            '-', 'Find', 'Replace','SelectAll','Scayt',
-            '-', 'BulletedList', 'NumberedList',
-            '-', 'Blockquote','-','JustifyLeft','JustifyCenter','JustifyRight','BidiLtr', 'BidiRtl',
-            '-', 'Image', 'Link', 'Unlink', 'Anchor', 'HorizontalRule',
-            '-', 'Table', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe',
-            '-', 'Source','Undo', 'Redo',
-             'Format', 'Font', 'FontSize','TextColor', 'BGColor','Maximize', 'ShowBlocks','About'
-            ],
+        { 'name': 'document', 'items': [ 'Source', '-', 'Preview', '-', 'Templates' ] }, #// Defines toolbar group with name (used to create voice label) and items in 3 subgroups.
+        { 'name': 'clipboard', 'groups': [ 'clipboard', 'undo' ], 'items': [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+        { 'name': 'editing', 'groups': [ 'find', 'selection', 'spellchecker' ], 'items': [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+        { 'name': 'insert', 'items': [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+        '/',                                                                                    #// Line break - next group will be placed in new line.
+        { 'name': 'basicstyles', 'groups': [ 'basicstyles', 'cleanup' ], 'items': [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+        { 'name': 'links', 'items': [ 'Link', 'Unlink', 'Anchor' ] },
+        { 'name': 'paragraph', 'groups': [ 'list', 'indent', 'blocks', 'align', 'bidi' ], 'items': [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+        '/',
+        { 'name': 'styles', 'items': [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+        { 'name': 'colors', 'items': [ 'TextColor', 'BGColor' ] },
+        { 'name': 'tools', 'items': [ 'Maximize', 'ShowBlocks' ] },
+        { 'name': 'others', 'items': [ '-' ] },
+        { 'name': 'about', 'items': [ 'About' ] }
         ],
     'filebrowserWindowWidth': 940,
     'filebrowserWindowHeight': 450,
