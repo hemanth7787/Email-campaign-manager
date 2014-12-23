cd4d8e61f17cfe34d4279785fcc2b51ab591ec03
Hemanth <hemanth7787@gmail.com>
2014-12-23 14:58:21 +0530

    Ckeditor downgraded and settings.py modified

--- email_campaign_manager/settings.py
+++ email_campaign_manager/settings.py
@@ -162,7 +162,7 @@ STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static1/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
